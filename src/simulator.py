dc87ce44ba877d56073a673403c8e40ea6404020
D4rkingPoint <Alvaro.pozo@usm.cl>
2024-11-05 18:24:44 -0300

    Update seleccionar estacionamiento
    seleccion estacionamiento

--- src/simulator.py
+++ src/simulator.py
@@ -2,6 +2,8 @@ import arcade
 from car import Car
 from cell import Cell, ParkingLot
 import random
+import tkinter as tk
+from tkinter import filedialog
 
 # Constants
 SCREEN_WIDTH = 1000
@@ -89,35 +91,50 @@ class Window(arcade.Window):
         self.car.move()
 
     def load_map(self):
+        # Crear una ventana de diálogo para seleccionar un archivo
+        root = tk.Tk()
+        root.withdraw()  # Ocultar la ventana principal de Tkinter
+        file_path = filedialog.askopenfilename(filetypes=[("Simulation Files", "*.sim")])
+
+        if not file_path:
+            print("No file selected")
+            return
+
+        # Crear la cuadrícula de celdas
         grid = []
         for i in range(SCREEN_WIDTH // 20):
             grid.append([])
             for j in range(SCREEN_HEIGHT // 20):
                 grid[i].append(Cell(arcade, i * 20 + 10, j * 20 + 10, temporal=True))
-        with open("map.sim", "r") as f:
-            for line in f:
-                i, j, direction = line.strip().split()
-                if direction == 'up':
-                    grid[int(i)][int(j)].connect(grid[int(i)][int(j) + 1])
-                elif direction == 'down':
-                    grid[int(i)][int(j)].connect(grid[int(i)][int(j) - 1])
-                elif direction == 'left':
-                    grid[int(i)][int(j)].connect(grid[int(i) - 1][int(j)])
-                elif direction == 'right':
-                    grid[int(i)][int(j)].connect(grid[int(i) + 1][int(j)])
-                elif direction == 'intersection':
-                    grid[int(i)][int(j)].connect(grid[int(i)][int(j) + 1])
-                    grid[int(i)][int(j)].connect(grid[int(i)][int(j) - 1])
-                    grid[int(i)][int(j)].connect(grid[int(i) - 1][int(j)])
-                    grid[int(i)][int(j)].connect(grid[int(i) + 1][int(j)])
-                elif direction == 'parking':
-                    grid[int(i)][int(j)] = ParkingLot(arcade, int(i) * 20 + 10, int(j) * 20 + 10)
-                    grid[int(i)][int(j)].connect(grid[int(i)][int(j) + 1])
-                    grid[int(i)][int(j)].connect(grid[int(i)][int(j) - 1])
-                    grid[int(i)][int(j)].connect(grid[int(i) - 1][int(j)])
-                    grid[int(i)][int(j)].connect(grid[int(i) + 1][int(j)])
-                self.cells.append(grid[int(i)][int(j)])
-                grid[int(i)][int(j)].save()
+
+        # Cargar el mapa desde el archivo seleccionado
+        try:
+            with open(file_path, "r") as f:
+                for line in f:
+                    i, j, direction = line.strip().split()
+                    if direction == 'up':
+                        grid[int(i)][int(j)].connect(grid[int(i)][int(j) + 1])
+                    elif direction == 'down':
+                        grid[int(i)][int(j)].connect(grid[int(i)][int(j) - 1])
+                    elif direction == 'left':
+                        grid[int(i)][int(j)].connect(grid[int(i) - 1][int(j)])
+                    elif direction == 'right':
+                        grid[int(i)][int(j)].connect(grid[int(i) + 1][int(j)])
+                    elif direction == 'intersection':
+                        grid[int(i)][int(j)].connect(grid[int(i)][int(j) + 1])
+                        grid[int(i)][int(j)].connect(grid[int(i)][int(j) - 1])
+                        grid[int(i)][int(j)].connect(grid[int(i) - 1][int(j)])
+                        grid[int(i)][int(j)].connect(grid[int(i) + 1][int(j)])
+                    elif direction == 'parking':
+                        grid[int(i)][int(j)] = ParkingLot(arcade, int(i) * 20 + 10, int(j) * 20 + 10)
+                        grid[int(i)][int(j)].connect(grid[int(i)][int(j) + 1])
+                        grid[int(i)][int(j)].connect(grid[int(i)][int(j) - 1])
+                        grid[int(i)][int(j)].connect(grid[int(i) - 1][int(j)])
+                        grid[int(i)][int(j)].connect(grid[int(i) + 1][int(j)])
+                    self.cells.append(grid[int(i)][int(j)])
+                    grid[int(i)][int(j)].save()
+        except Exception as e:
+            print(f"Error loading map: {e}")
 
 
 def main():
